3386dcd3c4c5969be8623cf421e6dad88919074f
IgorPatrykPraktyki <115373289+Damiana214155255@users.noreply.github.com>
2023-02-06 11:26:21 +0100

    a

--- script.js
+++ script.js
@@ -1,6 +1,66 @@
- function openForm() {
-      document.getElementById("popupForm").style.display = "block";
+var btn = document.getElementById("test");
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+if (event.target == modal) {
+modal.style.display = "none";
+}
+}
+
+// Initialize i18next
+i18next.init({
+  lng: 'pl',
+  resources: {
+    pl: {
+      translation: {
+        "header_about": "O mnie",
+        "header_projects": "Projekty",
+        "header_skills": "Umiejętności",
+        "header_contact": "Kontakt"
+      }
+    },
+    en: {
+      translation: {
+        "header_about": "About",
+        "header_projects": "Projects",
+        "header_skills": "Skills",
+        "header_contact": "Contact"
+      }
+    },
+    ru: {
+      translation: {
+        "header_about": "Обо мне",
+        "header_projects": "Проекты",
+        "header_skills": "Навыки",
+        "header_contact": "Контакты"
+      }
     }
-function closeForm() {
-      document.getElementById("popupForm").style.display = "none";
-    }
+  }
+});
+
+// Translate the menu items
+document.querySelectorAll('a').forEach(function(el) {
+  el.innerHTML = i18next.t(`header_${el.innerHTML.toLowerCase()}`);
+});
+
+// Add a language switcher
+var lngList = ['pl', 'en', 'ru'];
+var lngIndex = 0;
+document.querySelector('#language-switch').addEventListener('click', function() {
+  lngIndex = (lngIndex + 1) % lngList.length;
+  i18next.changeLanguage(lngList[lngIndex]);
+});
